55dd4e11087b02e2f2af92d1090aca9aa9b97a2d
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-11-02 15:36:59 -0400

    Clear up testimonial component

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -6,11 +6,6 @@ const Testimonial = () => {
           <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
           <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
           <div className="mx-auto max-w-2xl lg:max-w-4xl">
-            <img
-              alt="Logo"
-              src="https://tailwindui.com/plus/img/logos/workcation-logo-indigo-600.svg"
-              className="mx-auto h-8"
-            />
             <figure className="mt-10">
               <blockquote className="text-center text-xl/8 font-semibold text-gray-900 sm:text-2xl/9">
                 <p>
